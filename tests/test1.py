4942db86e023ed7d4df279a858636d3f08dfefe9
boolman <boolman@gmail.com>
2019-08-16 14:03:05 +0200

    Changed cronjob

--- tests/test1.py
+++ tests/test1.py
@@ -3,6 +3,7 @@ import HtmlTestRunner
 import subprocess
 import os
 from pathlib import Path
+import nmap
 
 
 USERNAME = os.getenv('USERNAME', 'ubuntu')
@@ -19,7 +20,7 @@ def RunCmd(cmd):
     return p.returncode
 
 
-class CourseOne(unittest.TestCase):
+class Lektion1_uppg1(unittest.TestCase):
 
     def test_installed_pkgs(self):
         cmd = ['/usr/lib/update-notifier/apt-check']
@@ -33,10 +34,10 @@ class CourseOne(unittest.TestCase):
         self.assertFalse(os.path.isfile('/var/run/reboot-required'))
 
 
-class CourseTwo(unittest.TestCase):
+class Lektion1_uppg2(unittest.TestCase):
     def setUp(self):
-        self.SYMLINK_SRC = '/opt/vault-0.11.5/vault'
-        self.SYMLINK_DEST = '/usr/local/bin/vault'
+        self.SYMLINK_SRC = f'/home/{USERNAME}/linuxserver2/reports'
+        self.SYMLINK_DEST = '/var/www/html/reports'
         self.SUBDIR = 'Downloads'
         self.CRONJOB_FILE = '/etc/cron.d/first_cronjob'
 
@@ -57,17 +58,17 @@ class CourseTwo(unittest.TestCase):
     def test_symlink_exists(self):
         self.assertTrue(Path(self.SYMLINK_DEST).is_symlink())
         self.assertEqual(os.readlink(self.SYMLINK_DEST), self.SYMLINK_SRC)
-        self.assertTrue(os.path.isfile(self.SYMLINK_SRC))
+        self.assertTrue(os.path.isdir(self.SYMLINK_SRC))
 
 
     def test_cronjob(self):
         self.assertTrue(os.path.isfile(self.CRONJOB_FILE))
         with open(self.CRONJOB_FILE, 'r') as f:
             content = f.read().strip()
-        self.assertEqual(content, f"* * * * * {USERNAME} echo hello world")
+        self.assertEqual(content, f"*/5 8-17 * * 1-6 {USERNAME} echo hello world")
 
 
-class CourseThree(unittest.TestCase):
+class Lektion2_uppg1(unittest.TestCase):
     def setUp(self):
         pass
 
@@ -86,6 +87,41 @@ class CourseThree(unittest.TestCase):
                              stderr=subprocess.PIPE)
         output, err = p.communicate()
         self.assertTrue(len(output.split('\n')) > 0)
+
+
+class LektionX_uppg1(unittest.TestCase):
+    """
+      scan kvm vm's for running services
+      [ ftp, smtp, http ]
+      'product' is found with nmap -sV -p <port> <ip>
+    """
+    def setUp(self):
+        self.nm = nmap.PortScanner()
+        self.nm.scan(hosts='127.0.0.1/32', ports='21,25,80')
+        self.hosts = {}
+        for host in self.nm.all_hosts():
+            if self.nm[host].has_tcp(21):
+                self.hosts['ftp'] = host
+            if self.nm[host].has_tcp(25):
+                self.hosts['smtp'] = host
+            if self.nm[host].has_tcp(80):
+                self.hosts['http'] = host
+
+
+    def test_vm_ftp(self):
+        self.assertTrue('ftp' in self.hosts)
+        self.assertTrue('FTP' in self.nm[self.hosts['ftp']]['tcp'][21]['product'])
+
+    def test_vm_smtp(self):
+        self.assertTrue('smtp' in self.hosts)
+        self.assertTrue('smtp' in self.nm[self.hosts['smtp']]['tcp'][25]['product'])
+
+
+    def test_vm_http(self):
+        self.assertTrue('http' in self.hosts)
+        self.assertTrue('http' in self.nm[self.hosts['http']]['tcp'][80]['product'])
+
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(
